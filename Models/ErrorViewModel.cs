7a2e9f852e7cec7cacb23453def0eb92ac2284fe
kckracker <caseyholland972@gmail.com>
2021-07-07 11:54:24 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Recipes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
